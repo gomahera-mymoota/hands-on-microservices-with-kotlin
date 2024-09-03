91ad59621125004cbb9f3205abceedf693cb41e2
gomahera-mymoota <iceman4u@naver.com>
2024-09-03 17:35:20 +0900

    01: p124 - RestController 만들기

---
+++ chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
@@ -0,0 +1,12 @@
+package com.microservices.chapter03
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@RestController
+class CustomerController {
+    
+    @RequestMapping(value = arrayOf("/customer"), method = arrayOf(RequestMethod.GET))
+    fun getCustomer() = "hello from a controller"
+}
