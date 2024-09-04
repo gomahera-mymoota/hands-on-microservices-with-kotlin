b522f4c7bc7bd5e3a8f049edd2212ef8cd7448b5
gomahera-mymoota <iceman4u@naver.com>
2024-09-04 14:18:58 +0900

    03-03 [HTTP 동사 처리] 동사와 매핑 애너테이션 사용

--- chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
+++ chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.concurrent.ConcurrentHashMap
 
@@ -15,21 +19,21 @@ class CustomerController {
     @Autowired
     lateinit var customers: ConcurrentHashMap<Int, Customer>
 
-    @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
+    @GetMapping(value = arrayOf("/customers"))
     fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
 
-    @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.GET))
+    @GetMapping(("/customer/{id}"))
     fun getCustomer(@PathVariable id: Int) = customers[id]
 
-    @RequestMapping(value = arrayOf("/customer/"), method = arrayOf(RequestMethod.POST))
+    @PostMapping("/customer/")
     fun createCustomer(@RequestBody customer: Customer) {
         customers[customer.id] = customer
     }
 
-    @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.DELETE))
+    @DeleteMapping("/customer/{id}")
     fun deleteCustomer(@PathVariable id: Int) = customers.remove(id)
 
-    @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.PUT))
+    @PutMapping("/customer/{id}")
     fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer) {
         customers.remove(id)
         customers[customer.id] = customer
