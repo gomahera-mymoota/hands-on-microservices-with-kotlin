3affbac02774d15dd697c37dfab08bbedd25cd3a
gomahera-mymoota <iceman4u@naver.com>
2024-09-04 13:45:53 +0900

    03-03 [HTTP 동사 처리] HTTP POST 처리

--- chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
+++ chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.concurrent.ConcurrentHashMap
 
@@ -14,13 +15,15 @@ class CustomerController {
     @Autowired
     lateinit var customers: ConcurrentHashMap<Int, Customer>
 
+    @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
+    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
+
     @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.GET))
     fun getCustomer(@PathVariable id: Int) = customers[id]
 
-    @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
-    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
-        customers.filter { 
-            it.value.name.contains(nameFilter, true)
-         }.map(Map.Entry<Int, Customer>::value).toList()
+    @RequestMapping(value = arrayOf("/customer/"), method = arrayOf(RequestMethod.POST))
+    fun createCustomer(@RequestBody customer: Customer) {
+        customers[customer.id] = customer
+    }
     
 }
