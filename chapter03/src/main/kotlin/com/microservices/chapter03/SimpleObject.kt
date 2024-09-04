c89ff6d956ea9cbe456163aa22fe19a08c70efdc
gomahera-mymoota <iceman4u@naver.com>
2024-09-04 17:29:36 +0900

    03 JSON으로 작업하기 [직렬화 이해] 간단한 객체 이용 - get으로 시작하는 public 메소드 사용

--- chapter03/src/main/kotlin/com/microservices/chapter03/SimpleObject.kt
+++ chapter03/src/main/kotlin/com/microservices/chapter03/SimpleObject.kt
@@ -4,4 +4,7 @@ class SimpleObject {
     public val name = "hello"
     private val place = "world"
     private val planet = "earth"
+
+    public fun getPlace() = place
+    public fun getLocation() = planet
 }
