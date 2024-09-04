a8ec48a84dd6269eb51084ef9d9d5391b1b77a2d
gomahera-mymoota <iceman4u@naver.com>
2024-09-04 12:07:35 +0900

    03-02 [경로와 요청 매개 변수] 요청 매개 변수 정의: 요청 매개 변수로 고객 이름 필터링

--- chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
+++ chapter03/src/main/kotlin/com/microservices/chapter03/CustomerController.kt
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.concurrent.ConcurrentHashMap
 
@@ -17,6 +18,9 @@ class CustomerController {
     fun getCustomer(@PathVariable id: Int) = customers[id]
 
     @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
-    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
+        customers.filter { 
+            it.value.name.contains(nameFilter, true)
+         }.map(Map.Entry<Int, Customer>::value).toList()
     
 }
