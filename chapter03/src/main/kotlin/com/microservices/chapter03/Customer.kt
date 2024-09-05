acd20fc6cd35c5bc2a052dbda60c86a8c877706c
gomahera-mymoota <iceman4u@naver.com>
2024-09-05 10:11:06 +0900

    03 JSON으로 작업하기 [직렬화 이해] 널 값 처리 - 옵셔널 필드 직렬화하지 않기 - 데이터 클래스에 Jackson 애너테이션 사용

--- chapter03/src/main/kotlin/com/microservices/chapter03/Customer.kt
+++ chapter03/src/main/kotlin/com/microservices/chapter03/Customer.kt
@@ -1,5 +1,9 @@
 package com.microservices.chapter03
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+
+@JsonInclude(Include.NON_NULL)
 data class Customer(var id: Int = 0, val name: String = "",
     var telephone: Telephone? = null) {
 
