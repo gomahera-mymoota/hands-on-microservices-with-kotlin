355bef8cc608fd8ba9b33934089936acb1521e41
gomahera-mymoota <iceman4u@naver.com>
2024-09-06 10:42:49 +0900

    04 스프링 웹플럭스의 이해 [RestController 추가] - ResponseEntity 사용하여 반환

--- chapter04/src/main/kotlin/com/microservices/chapter04/CustomerController.kt
+++ chapter04/src/main/kotlin/com/microservices/chapter04/CustomerController.kt
@@ -3,11 +3,15 @@ package com.microservices.chapter04
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 
 @RestController
 class CustomerController {
 
     @GetMapping("/customer/{id}")
-    fun getCustomer(@PathVariable id: Int) = Customer(id, "customer $id")
-    
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer> {
+        return ResponseEntity(Customer(id, "customer $id"), HttpStatus.OK)
+    }
+
 }
