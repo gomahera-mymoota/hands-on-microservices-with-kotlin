e232eda4dff9af5458a18c3f4e7cf81e1a9352fa
gomahera-mymoota <iceman4u@naver.com>
2024-09-09 12:06:11 +0900

    04 [함수형 웹 프로그래밍 사용하기] 리액티브 서비스 사용하기 - notfound() 헬퍼 함수를 사용한 404 NOT FOUND 응답

--- chapter04/src/main/kotlin/com/microservices/chapter04/CustomerHandler.kt
+++ chapter04/src/main/kotlin/com/microservices/chapter04/CustomerHandler.kt
@@ -1,20 +1,17 @@
 package com.microservices.chapter04
 
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import org.springframework.web.reactive.function.server.ServerResponse.status
 import org.springframework.web.reactive.function.server.ServerResponse.notFound
-import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.BodyInserters.fromValue
 
 @Component
 class CustomerHandler(val customerService: CustomerService) {
 
     fun get(serverRequest: ServerRequest) =
-        customerService.getCustomer(serverRequest.pathVariable("id").toInt())   // ok().body()가 flatMap()으로 들어감에 유의
-            .flatMap { ok().body(fromValue(it)) }   // getCustomer가 Mono<Customer>를 반환하기 때문에 map을 사용하면 Mono<Mono<Customer>>가 반환되므로 flatMap을 사용함
-                                                    // flatMap()을 통해 Mono<Customer>가 Mono<Response>로 변환
+        customerService.getCustomer(serverRequest.pathVariable("id").toInt())
+            .flatMap { ok().body(fromValue(it)) }
+            .switchIfEmpty(notFound().build())      // notfound() 헬퍼 함수를 이용한 404 NOT FOUND 응답
 
 }
