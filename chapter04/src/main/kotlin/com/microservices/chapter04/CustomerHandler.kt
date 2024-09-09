8e2229e463923139c6dfc95fb065ff5ab34a904e
gomahera-mymoota <iceman4u@naver.com>
2024-09-09 12:21:10 +0900

    04 [함수형 웹 프로그래밍 사용하기] 리액티브 서비스 사용하기 - status() 함수를 이용한 404 NOT FOUND 응답

--- chapter04/src/main/kotlin/com/microservices/chapter04/CustomerHandler.kt
+++ chapter04/src/main/kotlin/com/microservices/chapter04/CustomerHandler.kt
@@ -1,9 +1,10 @@
 package com.microservices.chapter04
 
 import org.springframework.stereotype.Component
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import org.springframework.web.reactive.function.server.ServerResponse.notFound
+import org.springframework.web.reactive.function.server.ServerResponse.status
 import org.springframework.web.reactive.function.BodyInserters.fromValue
 
 @Component
@@ -12,6 +13,6 @@ class CustomerHandler(val customerService: CustomerService) {
     fun get(serverRequest: ServerRequest) =
         customerService.getCustomer(serverRequest.pathVariable("id").toInt())
             .flatMap { ok().body(fromValue(it)) }
-            .switchIfEmpty(notFound().build())      // notfound() 헬퍼 함수를 이용한 404 NOT FOUND 응답
+            .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())    // status() 함수를 이용한 404 NOT FOUND 응답
 
 }
