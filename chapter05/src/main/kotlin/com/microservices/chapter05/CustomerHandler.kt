502948bae78fec59f3946a2fba37b0a3981b7a09
gomahera-mymoota <iceman4u@naver.com>
2024-09-11 14:17:23 +0900

    05 [CRUD 연산을 위한 RESTful API] GET을 READ로 매핑

---
+++ chapter05/src/main/kotlin/com/microservices/chapter05/CustomerHandler.kt
@@ -0,0 +1,17 @@
+package com.microservices.chapter05
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromValue
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.ServerResponse.notFound
+
+
+@Component
+class CustomerHandler(val customerService: CustomerService) {
+
+    fun get(serverRequest: ServerRequest) =
+        customerService.getCustomer(serverRequest.pathVariable("id").toInt())
+            .flatMap { ok().body(fromValue(it)) }
+            .switchIfEmpty(notFound().build())
+}
---
+++ chapter05/src/main/kotlin/com/microservices/chapter05/CustomerRouter.kt
@@ -0,0 +1,18 @@
+package com.microservices.chapter05
+
+import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.server.router
+
+
+@Component
+class CustomerRouter(private val customerHandler: CustomerHandler) {
+
+    @Bean
+    fun customerRoutes() = router {
+        "/customer".nest {
+            GET("/{id}", customerHandler::get)
+        }
+    }
+    
+}
