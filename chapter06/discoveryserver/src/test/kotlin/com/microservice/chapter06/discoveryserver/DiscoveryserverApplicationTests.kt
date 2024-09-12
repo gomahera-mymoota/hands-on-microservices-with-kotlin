28460d1f2be82b3275c25dbffbf09cbc12fc8e59
gomahera-mymoota <iceman4u@naver.com>
2024-09-12 11:19:57 +0900

    06 [서비스 탐색] 서비스 탐색 서버 만들기

---
+++ chapter06/discoveryserver/src/test/kotlin/com/microservice/chapter06/discoveryserver/DiscoveryserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.chapter06.discoveryserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
