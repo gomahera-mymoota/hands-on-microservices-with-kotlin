4528f3b3b643351009122f66aba1c38aa9fa66ad
gomahera-mymoota <iceman4u@naver.com>
2024-09-14 00:00:16 +0900

    06 [게이트웨이] 게이트웨이 만들기

---
+++ chapter06/gateway/src/main/kotlin/com/microservices/chapter06/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.microservices.chapter06.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
